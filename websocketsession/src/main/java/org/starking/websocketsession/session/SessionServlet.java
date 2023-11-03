fea935c4b963374f6631beb702ca3337b8b6a990
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-03 20:06:43 -0300

    Criando class de session

---
+++ websocketsession/src/main/java/org/starking/websocketsession/session/SessionServlet.java
@@ -0,0 +1,25 @@
+package org.starking.websocketsession.session;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * @author pedroRhamon
+ */
+public class SessionServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+	
+
+}
